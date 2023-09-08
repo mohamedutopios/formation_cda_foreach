458fc648c15b895d6f2a66d0f497881c249b816a
mohamedutopios <mohamed@utopios.net>
2023-09-08 16:26:26 +0200

    add demo interface

--- java-demo/src/main/java/org/example/poo/datastructure/Main.java
+++ java-demo/src/main/java/org/example/poo/datastructure/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         ElectricCar electricCar = new ElectricCar("blue");
-        ServiceEngine.moveVehicule(electricCar);
+        //ServiceEngine.moveVehicule(electricCar);
+        ServiceEngine.test();
     }
 }
--- java-demo/src/main/java/org/example/poo/datastructure/ServiceEngine.java
+++ java-demo/src/main/java/org/example/poo/datastructure/ServiceEngine.java
@@ -1,5 +1,9 @@
 package org.example.poo.datastructure;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ServiceEngine {
 
     public static void moveVehicule(Vehicle vehicle) {
@@ -18,6 +22,42 @@ public class ServiceEngine {
         }
 
     }
+    public static void test(){
 
+        ElectricCar electricCar = new ElectricCar("jaune");
+        System.out.println(electricCar);
+        MotorCycle motorCycle = new MotorCycle();
+        motorCycle.setCouleur("gris");
+        motorCycle.setNbreRoue(2);
+
+        PetrolCar petrolCar = new PetrolCar("blue");
+        petrolCar.setNbreRoue(4);
+
+        SuperCar superCar = new SuperCar();
+
+        Engine[] engines = {electricCar,motorCycle,petrolCar};
+        ElectricCar[] electricCars = {electricCar};
+
+       /* for (Engine v:engines) {
+            if(v instanceof ElectricCar){
+                ((ElectricCar) v).drive();
+            }
+
+        }*/
+
+        System.out.println(Arrays.asList(engines));
+
+
+        /*List<Engine> engineList = new ArrayList<>();
+        engineList.add(electricCar);
+        engineList.add(motorCycle);
+        engineList.add(petrolCar);*/
+
+       // engineList.stream().forEach(System.out::println);
+
+        Vehicle[] vehicles = {electricCar,motorCycle,petrolCar,superCar};
+
+        System.out.println(Arrays.asList(vehicles));
+    }
 
 }
