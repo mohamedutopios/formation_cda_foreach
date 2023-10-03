fa9ac528bc504a1026746e6fd0223c325fbe18b9
Christophe <christophe.delory@labom2iformation.fr>
2023-10-03 16:58:30 +0200

    debut correction hotel

--- hotel/src/main/java/org/example/classes/Hotel.java
+++ hotel/src/main/java/org/example/classes/Hotel.java
@@ -18,6 +18,50 @@ public class Hotel {
     }
 
     private ArrayList<Chambre> genListChambres(){
-        return new ArrayList<>();
+        ArrayList<Chambre> genListeChambres = new ArrayList<>();
+        int min = 10;
+        int max = 50;
+        int numChambre = 1;
+        int randomNumber =(int) (Math.random() * ((max - min) + 1) + min);
+        for (int i= 1 ; i<randomNumber;i++){
+            int randomNbLits = (int) (Math.random() * ((4 - 1) + 1) + 1);
+            double randomTarif = Math.random() * (50.0 - 30.0) + 30.0;
+            double randomTarifArrondi = Math.round(randomTarif * 10.0) / 10.0;
+            Chambre chambre = new Chambre(numChambre++,randomNbLits,randomTarifArrondi);
+            genListeChambres.add(chambre);
+        }
+        return genListeChambres;
+    }
+
+    public ArrayList<Client> getListeClients() {
+        return listeClients;
+    }
+
+    public void setListeClients(ArrayList<Client> listeClients) {
+        this.listeClients = listeClients;
+    }
+
+    public ArrayList<Chambre> getListeChambres() {
+        return listeChambres;
+    }
+
+    public void setListeChambres(ArrayList<Chambre> listeChambres) {
+        this.listeChambres = listeChambres;
+    }
+
+    public ArrayList<Reservation> getListeReservations() {
+        return listeReservations;
+    }
+
+    public void setListeReservations(ArrayList<Reservation> listeReservations) {
+        this.listeReservations = listeReservations;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
     }
 }
--- hotel/src/main/java/org/example/ihm/IhmConsole.java
+++ hotel/src/main/java/org/example/ihm/IhmConsole.java
@@ -1,8 +1,24 @@
 package org.example.ihm;
 
+import org.example.classes.Chambre;
+import org.example.classes.Hotel;
+
+import java.util.Scanner;
+
 public class IhmConsole {
 
+    private static Hotel hotel;
+
+    private static Scanner scanner = new Scanner(System.in);
+
+
     public static void start(){
-        System.out.println("Debut programme Hotel");
+        System.out.println("Quelle est le nom de l' Hotel");
+        String nomHotel = scanner.nextLine();
+        hotel = new Hotel(nomHotel);
+        System.out.println(hotel.getNom());
+        for (Chambre chambre: hotel.getListeChambres()){
+            System.out.println(chambre);
+        }
     }
 }
