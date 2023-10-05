1cd7c8c2c12d2ee3db45aabbcaf7401ac03094f7
Christophe <christophe.delory@labom2iformation.fr>
2023-10-05 11:21:04 +0200

    debut demos stream

--- java-demo/src/main/java/org/example/streams/Demo1.java
+++ java-demo/src/main/java/org/example/streams/Demo1.java
@@ -2,9 +2,7 @@ package org.example.streams;
 
 import org.example.array.Array;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Demo1 {
@@ -39,6 +37,44 @@ public class Demo1 {
         System.out.println();
         System.out.println("Sorted inverse:");
         prenoms.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+
+
+        List<Employe> liste = new ArrayList<>();
+
+        liste.add(new Employe(1,Genre.HOMME,176,3500));
+        liste.add(new Employe(2,Genre.HOMME,180,1900));
+        liste.add(new Employe(3,Genre.FEMME,190,2600));
+        liste.add(new Employe(4,Genre.HOMME,176,2400));
+        liste.add(new Employe(5,Genre.FEMME,156,2200));
+        liste.add(new Employe(6,Genre.HOMME,176,2000));
+        liste.add(new Employe(7,Genre.HOMME,176,3500));
+
+
+        double salaireTotale = liste.stream()
+                .mapToDouble(e -> e.getSalaire())
+                .sum();
+
+        System.out.println("Total des salaire : "+salaireTotale);
+
+
+        Optional<Employe> employe = liste.stream()
+                .filter(p -> p.getTaille()>=170)
+                .findFirst();
+
+        System.out.println("Le premier employe qui fait au moins 176 cm est + "+employe.get());
+
+        int[] nbr = {1,4,5,7,3,60};
+
+        int somme = Arrays.stream(nbr).reduce(0,(a,b) -> a + b);
+        System.out.println("La somme du tableau nbr est de : "+somme);
+
+        List<Integer> nbrs = Arrays.asList(1,2,3,4,5);
+
+        List<Integer> pairs = nbrs.stream()
+                .filter(x -> x%2 == 0)
+                .toList();
+
+        pairs.stream().forEach(System.out::println);
 
 
 
---
+++ java-demo/src/main/java/org/example/streams/Genre.java
@@ -0,0 +1,7 @@
+package org.example.streams;
+
+public enum Genre {
+
+    HOMME,
+    FEMME
+}
