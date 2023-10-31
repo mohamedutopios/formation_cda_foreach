858215d923ca1f9adef16a2ba3a6e253112f8c4d
Christophe <christophe.delory@labom2iformation.fr>
2023-10-31 11:13:23 +0100

    suite demo

---
+++ java_spring/demos/demoFirst/src/main/java/com/example/demoFirst/controllers/CrudRestController.java
@@ -0,0 +1,41 @@
+package com.example.demoFirst.controllers;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/crud")
+public class CrudRestController {
+
+
+
+    @GetMapping // GET http://localhost:8081/api/v1/crud
+    public List<String> getAllPerson(){
+
+        return List.of("John Dupont","Maria Martez","Chloée Smith");
+    }
+
+    @GetMapping("/{id}")  // GET http://localhost:8081/api/v1/crud/x
+    public String gatOnePerson(@PathVariable int id){
+
+        return "Vous chercher la personne avec l'id : "+ id;
+    }
+
+    @PostMapping // POST http://localhost:8081/api/v1/crud
+    public String create(@RequestBody String message){
+        return "Votre message est :"+ message;
+    }
+
+    @DeleteMapping("/{id}")  // Delete http://localhost:8081/api/v1/crud/x
+    public String deletePerson(@PathVariable int id){
+        return "Vous chercher a supprimer la personne avec l'id : "+ id;
+    }
+
+    @PutMapping("/{id}")  // Put http://localhost:8081/api/v1/crud/x
+    public String updatePerson(@PathVariable int id){
+        return "Vous chercher a modifier la personne avec l'id : "+ id;
+    }
+
+}
