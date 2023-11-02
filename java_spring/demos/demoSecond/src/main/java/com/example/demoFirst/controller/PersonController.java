c21095b84f6276b349df62ad2f71e6b3969c23f1
Christophe <christophe.delory@labom2iformation.fr>
2023-11-02 10:36:43 +0100

    debut second demo

--- java_spring/demos/demoSecond/src/main/java/com/example/demoFirst/controller/PersonController.java
+++ java_spring/demos/demoSecond/src/main/java/com/example/demoFirst/controller/PersonController.java
@@ -1,8 +1,12 @@
 package com.example.demoFirst.controller;
 
 
+import com.example.demoFirst.model.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PersonController {
@@ -14,8 +18,18 @@ public class PersonController {
     }
 
     @GetMapping("/second")
-    public String secondPage(){
+    public String secondPage(Model model){
+        Person person = new Person();
+        person.setName("Mohamed");
+        model.addAttribute("personSubmit",person);
         return "pageB";
+    }
+
+    @PostMapping("/submitForm")
+    public String submitFormulaire(@ModelAttribute("personSubmit")Person person){
+        System.out.println(person);
+        System.out.println(person.getName());
+        return "redirect:/";
     }
 
 }
