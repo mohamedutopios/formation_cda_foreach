c4d47264c65381c924c66c32037ae160c30d773c
mohamedutopios <mohamed@utopios.net>
2023-11-27 15:14:53 +0100

    add

--- java_spring/demos/demo4/exercice3/src/main/java/com/example/demoFirst/repository/StudentRepository.java
+++ java_spring/demos/demo4/exercice3/src/main/java/com/example/demoFirst/repository/StudentRepository.java
@@ -4,7 +4,12 @@ import com.example.demoFirst.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    List<Student> findByLastName(String lastname);
+    List<Student> findByAgeGreaterThan(int age);
+
 
 }
